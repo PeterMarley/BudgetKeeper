262f0261423c96ad80b803ad9c692b73e28cd4ba
BigJeffTheChef <marleygpeter@gmail.com>
2022-04-19 17:19:37 +0100

    about to redesign month window

--- src/view/controllers/WindowMonthController.java
+++ src/view/controllers/WindowMonthController.java
@@ -8,6 +8,7 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import model.db.Constants;
 import model.domain.Month;
@@ -15,10 +16,53 @@ import model.domain.Transaction;
 import model.domain.Transaction.Type;
 
 public class WindowMonthController {
+	
+	/*
+	           <TableColumn fx:id="transactionsCash_date" prefWidth="75.0" text="C1" />
+          <TableColumn fx:id="transactionsCash_name" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsCash_value" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsCash_paid" prefWidth="75.0" text="C2" />
+        </columns>
+      </TableView>
+      <TableView fx:id="transactionsDirectDebit" layoutX="25.0" layoutY="566.0" prefHeight="113.0" prefWidth="825.0">
+         <columns>
+            <TableColumn fx:id="transactionsDirectDebit_date" prefWidth="75.0" text="C1" />
+            <TableColumn fx:id="transactionsDirectDebit_name" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsDirectDebit_value" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsDirectDebit_paid" prefWidth="75.0" text="C2" />
+         </columns>
+      </TableView>
+      <TableView fx:id="transactionsStandingOrder" layoutX="33.0" layoutY="699.0" prefHeight="106.0" prefWidth="825.0">
+         <columns>
+            <TableColumn fx:id="transactionsStandingOrder_date" prefWidth="75.0" text="C1" />
+            <TableColumn fx:id="transactionsStandingOrder_name" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsStandingOrder_value" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsStandingOrder_paid" prefWidth="75.0" text="C2" />
+         </columns>
+      </TableView>
+      <TableView fx:id="transactionsBankTransfer" layoutX="25.0" layoutY="445.0" prefHeight="106.0" prefWidth="825.0">
+         <columns>
+            <TableColumn fx:id="transactionsBankTransfer_date" prefWidth="75.0" text="C1" />
+            <TableColumn fx:id="transactionsBankTransfer_name" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsBankTransfer_value" prefWidth="75.0" text="C2" />
+            <TableColumn fx:id="transactionsBankTransfer_paid" prefWidth="75.0" text="C2" />
+         </columns> 
+	 */
 	private Month month;
 
+	// cash table
 	@FXML
 	private TableView<Transaction> transactionsCash;
+	@FXML
+	private TableColumn<Transaction, LocalDate> transactionsCash_date;
+	@FXML
+	private TableColumn<Transaction, String> transactionsCash_name;
+	@FXML
+	private TableColumn<Transaction, Double> transactionsCash_value;
+	@FXML
+	private TableColumn<Transaction, Boolean> transactionsCash_paid;
+	
+	// direct debit table
 	@FXML
 	private TableView<Transaction> transactionsDirectDebit;
 	@FXML
@@ -71,7 +115,7 @@ public class WindowMonthController {
 		private SimpleStringProperty name;
 		private SimpleBooleanProperty paid;
 
-		public TransactionModel(Transaction t) {
+		protected TransactionModel(Transaction t) {
 			this.setIncome(t.isIncome());
 			this.setDate(t.getDate());
 			this.setType(t.getType());
